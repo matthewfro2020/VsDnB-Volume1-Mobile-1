d55967cdfcba0faa5f7c47119a6b9d1cf02f7314
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-02 09:35:33 -0500

    Update FlxZSprite.hx

--- source/util/FlxZSprite.hx
+++ source/util/FlxZSprite.hx
@@ -2,13 +2,8 @@ package util;
 
 import flixel.FlxSprite;
 
-/**
- * Extension wrapper to give ANY FlxSprite a safe z-index metadata field.
- * Does NOT modify Flixel internals.
- */
 class FlxZSprite extends FlxSprite
 {
-    /** Metadata only! Does NOT affect rendering unless you sort manually */
     public var zIndex:Int = 0;
 
     public function new(x:Float = 0, y:Float = 0)
